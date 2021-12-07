4dd93a3f92a41f6fe6cefe5c62886b0e06b169b0
Lee Juhyun <81246537+smilejulie0812@users.noreply.github.com>
2021-12-07 22:51:06 +0900

    Update 002.로또의최고순위와최저순위.js

--- JavaScript/002.로또의최고순위와최저순위.js
+++ JavaScript/002.로또의최고순위와최저순위.js
@@ -13,9 +13,9 @@ function solution(lottos, win_nums) {
     }
     // 이 부분 뭔가 엄청 바보같이 푼 느낌... 짧게 못줄이나?
     var best = ''; var worst = '';
-    if ( zero_count == 6) {
+    if ( zero_count == 6 ) {
         best = 1 ; worst = 6;
-    } else if ( count == 6) {
+    } else if ( count == 6 ) {
         best = 1 ; worst = 1;
     } else if ( count == 0 && zero_count == 0 ) {
         best = 6; worst = 6;
@@ -26,7 +26,7 @@ function solution(lottos, win_nums) {
     var answer = [ best, worst ];
     return answer;
 }
-// 가장 많은 좋아요를 많이 받은 답!
+// 가장 많은 좋아요를 받은 답!
 function solution(lottos, win_nums) {
     const rank = [6, 6, 5, 4, 3, 2, 1];
 
