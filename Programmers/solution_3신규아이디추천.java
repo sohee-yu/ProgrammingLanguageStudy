5a4e0ecb9e6039fc0c1b1eb37b3a6aee7559eec1
sohee <sohui0742@daum.net>
2021-12-17 23:11:45 +0900

    프로그래머스 신규아이디추천..수정중

---
+++ Programmers/solution_3신규아이디추천.java
@@ -0,0 +1,16 @@
+class Solution {
+     public String solution(String new_id) {
+        String answer = "";
+        
+
+
+        return answer;
+    }
+    //1. 모든 대문자를 대응되는 소문자로 치환
+    public static String step1(String id){        
+
+        id = id.toLowerCase(); //소문자치환
+
+        return id;        
+    }   
+}
